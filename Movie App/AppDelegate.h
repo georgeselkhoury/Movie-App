922770ac1f971e1ffc952978f2a35366a3fcea54
Georges El Khoury <elkhourygeorges@hotmail.com>
2015-09-23 19:07:31 -0700

    Initial Commit

---
+++ Movie App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie App
+//
+//  Created by Georges El Khoury on 9/23/15.
+//  Copyright © 2015 Attendible. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
